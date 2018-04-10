45f4d7cab1a75c15aea0e8882273951fb81ffd18
Gavi Rawson <gavirawson@gmail.com>
2018-04-10 15:36:39 -0400

    Updated install file

--- bash/install.sh
+++ bash/install.sh
@@ -1,6 +1,6 @@
 # setup workspace
 source /opt/ros/kinetic/setup.bash
-cd ~/Baxter-The-Pool-Whiz
+cd ~/Baxter-The-Pool-Wiz
 catkin build
 
 # install Baxter SDK dependencies
@@ -8,18 +8,18 @@ sudo apt-get update
 sudo apt-get install git-core python-argparse python-wstool python-vcstools python-rosdep ros-kinetic-control-msgs ros-kinetic-joystick-drivers
 
 # Install Baxter SDK
-cd ~/Baxter-The-Pool-Whiz/src
+cd ~/Baxter-The-Pool-Wiz/src
 wstool init .
 wstool merge https://raw.githubusercontent.com/RethinkRobotics/baxter/master/baxter_sdk.rosinstall
 wstool update
 
 # Build workspace
 source /opt/ros/kinetic/setup.bash
-cd ~/Baxter-The-Pool-Whiz
+cd ~/Baxter-The-Pool-Wiz
 catkin build
 
 # Configure Baxter Communication
-cd ~/Baxter-The-Pool-Whiz
+cd ~/Baxter-The-Pool-Wiz
 wget https://github.com/RethinkRobotics/baxter/raw/master/baxter.sh
 chmod u+x baxter.sh
 
@@ -31,13 +31,13 @@ vim baxter.sh
 sudo apt-get install gazebo7 ros-kinetic-qt-build ros-kinetic-gazebo-ros-control ros-kinetic-gazebo-ros-pkgs ros-kinetic-ros-control ros-kinetic-control-toolbox ros-kinetic-realtime-tools ros-kinetic-ros-controllers ros-kinetic-xacro python-wstool ros-kinetic-tf-conversions ros-kinetic-kdl-parser
 
 # Install Baxter simulator
-cd ~/Baxter-The-Pool-Whiz/src
+cd ~/Baxter-The-Pool-Wiz/src
 wstool merge https://raw.githubusercontent.com/RethinkRobotics/baxter_simulator/kinetic-devel/baxter_simulator.rosinstall
 wstool update
 
 # Build workspace
 source /opt/ros/kinetic/setup.bash
-cd ~/Baxter-The-Pool-Whiz
+cd ~/Baxter-The-Pool-Wiz
 catkin build
 cp src/baxter/baxter.sh .
 
